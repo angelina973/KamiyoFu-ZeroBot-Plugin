3603f0a3dc8535cacb566e5e72935b9d09bdadf9
Angelina973 <58808936+angelina973@users.noreply.github.com>
2022-07-28 13:43:25 +0800

    change url to fork

--- kanban/banner.go
+++ kanban/banner.go
@@ -12,7 +12,7 @@ var (
 		"* OneBot + ZeroBot + Golang + Angelina973mod",
 		"* Version 1.5.0-b5.rc1 - 2022-07-28 12:37:17 +0800 CST",
 		"* Copyright © 2020 - 2022 FloatTech. All Rights Reserved.",
-		"* Project: https://github.com/FloatTech/ZeroBot-Plugin",
+		"* Project: https://github.com/angelina973/ZeroBot-Plugin",
 	}
 	// Banner ...
 	Banner = strings.Join(info[:], "\n")
